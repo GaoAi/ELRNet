3cc1a699a836d4e31b3f7b99cb4ee09671dba567
GaoAi <1594879985@qq.com>
2023-04-11 13:34:18 +0800

    new commit

--- vis_loss_and_iou.py
+++ vis_loss_and_iou.py
@@ -9,10 +9,9 @@ val_IoU_result =[]
 
 json_filename = './v7_eca.json'
 
-# 读取json文件内容,返回字典格式
+
 with open(json_filename,'r',encoding='utf8')as fp:
     json_data = json.load(fp)
-    # print('这是文件中的json数据：',json_data)
     iteration_result = json_data["iteration"]
     train_loss_result = json_data["train_loss"]
     val_loss_result = json_data["val_loss"]
@@ -26,20 +25,17 @@ y2 = val_loss_result
 y3 = train_IoU_result
 y4 = val_IoU_result
 
-# #创建画布
+
 plt.figure()
-#绘制第一条数据线、线颜色、标签名字
+
 plt.plot(x1,y1,color='#828282',label='train loss',linewidth=2)
-#绘制第二条数据线、线颜色、标签名字
 plt.plot(x1,y2,color='#FF4500', label='val loss',linewidth=2)
-#绘制第三条数据线、线颜色、标签名字
 plt.plot(x1,y3,color='#0064FF', label='train IoU',linewidth=2)
-#绘制第四条数据线、线颜色、标签名字
 plt.plot(x1,y4,color='#FF9614', label='val IoU',linewidth=2)
 
-#显示标题
+
 # plt.title("loss_and_IoU")
-#显示图例
+
 plt.legend()
 
 # plt.xlabel('iteration')
